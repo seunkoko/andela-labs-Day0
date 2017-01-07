918cd159164a27cbfa3156843a357ea2b2e354ed
seunkoko <seunowonikoko@gmail.com>
2017-01-07 05:11:55 +0100

    App initialized

---
+++ labs.js
@@ -0,0 +1,19 @@
+'use strict'
+
+module.exports = {
+
+    findMinMax: function (arr) {
+	// TODO
+	return [];
+    },
+
+    fizzBuzz: function (n) {
+        // TODO
+	return '';
+    },
+
+    aritGeo: function (arr) {
+        // TODO
+	return 0;
+    }
+}
