338d8fc627b2ccfee4cfef17992f3109b5b3dc49
seunkoko <seunowonikoko@gmail.com>
2017-01-07 08:15:33 +0100

    all functions tested and tests updated

--- labs.js
+++ labs.js
@@ -1,33 +1,68 @@
-'use strict'
 
-module.exports = {
+	'use strict'
 
-    findMinMax: function (arr) {
-		let max = arr[0];
-		let min = arr[0];
+	module.exports = {
 
-		for (let i = 1; i < arr.length; i++){
-			if(max < arr[i]){
-				max = arr[i];
-			}else if(min > arr[i]){
-				min = arr[i];
+		findMinMax: function (arr) {
+			let max = arr[0];
+			let min = arr[0];
+
+			for (let i = 1; i < arr.length; i++) {
+				if (max < arr[i]) {
+					max = arr[i];
+				} else if (min > arr[i]) {
+					min = arr[i];
+				}
+			}
+			
+			if (min === max){
+				return [min];
+			} else {
+				return [min, max];
+			}
+		},
+
+		fizzBuzz: function (num) {
+			if ((num % 3 === 0) && (num % 5 === 0)) {
+				return "FizzBuzz";
+			} else if (num % 3 === 0) {
+				return "Fizz";
+			} else if (num % 5 === 0) {
+				return "Buzz";
+			} else {
+				return num;
+			}
+		},
+
+		aritGeo: function (arr) {
+			if (arr.length > 0) {
+				let isArithmetic = true;
+				let isGeometric = true;
+				let arithmeticDiff = arr[1] - arr[0];
+				let geometricDiff = arr[1] / arr[0];
+				
+				for (let i = 1; i < arr.length; i++) {
+					let testArithmetic = arr[i] - arr[i-1];
+					let testGeometric = arr[i] / arr[i-1];
+					
+					if (testArithmetic != arithmeticDiff) {
+						isArithmetic = false;
+					}
+					
+					if (testGeometric != geometricDiff) {
+						isGeometric = false;
+					}
+				}
+				
+				if (isArithmetic) {
+					return "Arithmetic";
+				} else if (isGeometric) {
+					return "Geometric";
+				} else {
+					return -1;
+				}
+			} else {
+				return 0;
 			}
 		}
-		
-		if (min === max){
-			return [min];
-		}else{
-			return [min, max];
-		}
-    },
-
-    fizzBuzz: function (n) {
-        // TODO
-	return '';
-    },
-
-    aritGeo: function (arr) {
-        // TODO
-	return 0;
-    }
-}
+	}
--- tests/findMinMaxTests.js
+++ tests/findMinMaxTests.js
@@ -18,12 +18,29 @@ describe('Min-Max Numbers in a List: ', function () {
       expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
     });
 
+    it('should return [5, 17] for [5, 17, 5, 5, 5, 5, 17]', function () 
+     {
+       expect(findMinMax([5, 17, 5, 5, 5, 5, 17])).toEqual([5, 17]);
+    });
+
   });
 
   describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
 
     it('should return [4] for [4, 4, 4, 4]', function () {
       expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
+    });
+
+  });
+
+  describe('Return the number in the list in a new list follows `[min, max]` when the array consists of negative numbers', function () {
+
+    it('should return [-25, -1] for [-4, -3, -9, -20, -25, -1]', function () {
+      expect(findMinMax([-4, -3, -9, -20, -25, -1])).toEqual([-25, -1]);
+    });
+
+     it('should return [-9, -4] for [-4, -5, -6, -7, -8, -9]', function () {
+      expect(findMinMax( [-4, -5, -6, -7, -8, -9])).toEqual([-9, -4]);
     });
 
   });
